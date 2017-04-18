929cd5f8bb0962fcb0b89a5947ce432bc5949717
Slepick <slepick4@gmail.com>
2017-04-18 15:27:43 +0300

    Вернул всё)

---
+++ RPG/RPG/RPG_Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG
+{
+    [Serializable]
+    class RPG_Character
+    {
+        private static uint nextID = 1;
+        public uint ID { get; set; }
+        private string name { get; set; }
+        public uint age { get; set; }
+        bool talkative;
+        bool walkable;
+        //начало положено
+        
+        
+    }
+}
