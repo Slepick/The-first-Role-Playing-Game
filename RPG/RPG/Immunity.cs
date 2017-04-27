ff1cd0eb451d2f5af253547ef0627281ff257484
Slepick <slepick4@gmail.com>
2017-04-27 22:37:37 +0300

    Vlad's part 1

---
+++ RPG/RPG/Immunity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RPG
+{
+    class Immunity : Spell
+    {
+        public Immunity(uint requiredMP, bool isSilent, bool isStaned) : base(requiredMP, isSilent, isStaned)
+        {
+        }
+
+        public override void Cast(RPG_Character character, uint power)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+       
+    }
+}
--- RPG/RPG/RPG_Character.cs
+++ RPG/RPG/RPG_Character.cs
@@ -8,23 +8,39 @@ namespace RPG
     [Serializable]
     class RPG_Character : IComparable
     {
+
+        event EventHandler<DeathRattleEventArgs> hpChange; //Создал событие!!!
+
         /// <summary>
         /// поля
         /// </summary>
+        /// 
         private static uint nextID = 1;
-        private uint ID { get; set; } 
+        private uint ID { get; set; }
         private string name { get; set; }                                           //имя       
-        enum condition { Normal, Weakened, Sick, Poisoned, Paralyzed, Dead };       //состояние
+        public enum condition { Normal, Weakened, Sick, Poisoned, Paralyzed, Dead };       //состояние
         public condition cond { get; set; }
         public bool talkative { get; set; }                                         //возможность разговаривать
         public bool walkable { get; set; }                                          //возможность двигаться
-        enum race { Human, Dwarf, Elf, Ork, Goblin };                               //раса
+        public enum race { Human, Dwarf, Elf, Ork, Goblin };                               //раса
         private race race_type { get; set; }
         private bool sex { get; set; }                                              //пол(женский false, мужской true)(а может enum?)
         public uint age { get; set; }                                               //возраст
-        public uint currrentHP { get; set; }                                        //текущее здоровье
+        public uint currentHP                                                   //текущее здоровье
+        {
+            get
+            {
+                return currentHP;
+            }
+            set
+            {
+                currentHP = value;
+                hpChange(this, new DeathRattleEventArgs(maxHP, currentHP));//когда оно происходит!!!
+            }
+        }
         public uint maxHP { get; set; }                                             //макс здоровье
         public uint Expirience { get; set; }                                        // опыт
+
 
 
         /// <summary>
@@ -40,6 +56,7 @@ namespace RPG
             sex = Sex;
             ID = nextID;
             nextID++;
+            this.hpChange += this.status_check;//подписался!!!на всякий случай в начале this написал,может не надо
         }
 
         /// <summary>
@@ -64,17 +81,28 @@ namespace RPG
         /// обработчик события
         /// </summary>
         /// <param name="sender"></param>
-        /// <param name="ev"></param>
-
-        private static void status_check(object sender, DeathRattleEventArgs ev)
+        /// <param name="ev"></param>        
+        private void status_check(object sender, DeathRattleEventArgs ev)//твой Обработчик события
         {
-            //вроде тут ничего
+            if ((ev.Heath >= 10) && (cond == condition.Weakened))
+            {
+                cond = condition.Normal;
+            }
+            if ((ev.Heath > 0) && (ev.Heath < 10) && (cond == condition.Normal))
+            {
+                cond = condition.Weakened;
+            }
+            if ((ev.Heath == 0) && (cond != condition.Dead))
+            {
+                cond = condition.Dead;
+            }
         }
+
 
         //ToString
         public override string ToString()
         {
-            return base.ToString() + ":\n " 
+            return base.ToString() + ":\n "
                 + "\n Идентификатор персонажа: " + ID.ToString()
                 + "\n Имя персонажа: " + name.ToString()
                 + "\n Состояние персонажа: " + cond.ToString()
@@ -83,7 +111,7 @@ namespace RPG
                 + "\n Раса персонажа: " + race_type.ToString()
                 + "\n Пол персонажа: " + sex.ToString()
                 + "\n Возраст персонажа: " + age.ToString()
-                + "\n Текущее здоровье персонажа: " + currrentHP.ToString()
+                + "\n Текущее здоровье персонажа: " + currentHP.ToString()
                 + "\n Максимальное здоровье персонажа: " + maxHP.ToString()
                 + "\n Опыт персонажа: " + Expirience.ToString();
         }
