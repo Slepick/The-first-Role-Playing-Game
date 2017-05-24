18570a5cd928a27883fe542a0061a9412c0233bb
Allowner <se1999va@tut.by>
2017-05-24 21:06:44 +0300

    наконец то я смог

---
+++ 2D RPG/Assets/Scripts/CharacterWieldingMagic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG
+{
+    public class CharacterWieldingMagic : RPG_Character
+    {
+        public uint currentMP { get; set; }                                        //текущая мана
+        public uint maxMP { get; set; }                                             //макс мана
+        public CharacterWieldingMagic(string Name, race R, bool Sex) : base(Name, R, Sex)
+        {
+            maxMP = 100; // Опять же, сколько по умолчанию
+            currentMP = maxMP;
+        }
+    }
+}
---
+++ 2D RPG/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    public int playerCurrentHealth;
+    public int playerMaxHealth;
+
+    public int playerCurrentMana;
+    public int playerMaxMana;
+    // Use this for initialization
+    void Start () {
+        playerCurrentHealth = playerMaxHealth;
+        playerCurrentMana = playerMaxMana;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerCurrentHealth == 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void HurtPlayer(int damageToGive)
+    {
+        playerCurrentHealth -= damageToGive;
+        if (playerCurrentHealth < 0)
+            playerCurrentHealth = 0;
+    }
+
+    public void UseMana(int mana)
+    {
+        playerCurrentMana -= mana;
+        if (playerCurrentMana < 0)
+            playerCurrentMana = 0;
+    }
+
+    public void SetMaxHealth()
+    {
+        playerCurrentHealth = playerMaxHealth;
+    }
+}
